48375b303c4d967bcf5a48afa11c2fd0fcca84fb
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2024-10-20 19:30:29 -0400

    fixed cors origing

--- src/containers/CommentsContainer.jsx
+++ src/containers/CommentsContainer.jsx
@@ -24,51 +24,50 @@ const CommentsContainer = ( {workOrder, user} )=> {
     const [guid, setGuid] = useState("")
 
     useEffect(()=>{ 
-
         const ws = wsRef.current
         const payload = commentsGetSetter({id: workOrderId, comments_length: 0 })
         dispatch(getFetchAction(payload))
-        ws.onopen = ()=>{
-        setGuid(Math.random().toString(36).substring(2,15)) 
-            ws.send(
-                JSON.stringify({
-                    command: 'subscribe',
-                    withCredentials: true,
-                    identifier: JSON.stringify({
-                        id: guid,
-                        work_order_id: workOrderId,
-                        channel: "CommentsChannel"
-                    })
-                })
-            )
-        }
+        // ws.onopen = ()=>{
+        // setGuid(Math.random().toString(36).substring(2,15)) 
+        //     ws.send(
+        //         JSON.stringify({
+        //             command: 'subscribe',
+        //             withCredentials: true,
+        //             identifier: JSON.stringify({
+        //                 id: guid,
+        //                 work_order_id: workOrderId,
+        //                 channel: "CommentsChannel"
+        //             })
+        //         })
+        //     )
+        // }
     
-        ws.onmessage = (e)=>{
-            const data = JSON.parse(e.data)
-            if(data.type === "ping") return
-            if(data.type === "welcome") return
-            if(data.type === "confirm_subscription") return
-            if (data.message?.from_create_comment){
-                const payload = commentGetSetter({workOrderId: workOrderId, id: data.message.id})
-                // dispatch(getFetchAction(payload))
-            } else if (data.message?.from_delete_comment){
-                dispatch(deleteComment(data.message?.comment_deleted))
-            } else if (data.message?.from_create_like_for_comment){
-                const response = data.message.response
-                dispatch(addOrRemoveLikesFromComment(JSON.parse(response)))
-            } else if (data.message?.from_delete_like_from_comment){
-                dispatch(addOrRemoveLikesFromComment(data.message.like_deleted)) 
-            }else  if (data.message?.from_delete_reply ){
-                dispatch(removeReplyFromComment(data.message.reply_deleted))
-            }else if(data.message?.from_create_reply){
-                dispatch(addNewReply(JSON.parse(data.message.reply)))
-            }else if(data.message?.from_create_like_for_reply) {
-                const response = data.message.response
-                dispatch(addOrRemoveLikesFromReply(JSON.parse(response)))
-            } else if (data.message?.from_delete_like_from_reply){
-                dispatch(addOrRemoveLikesFromReply(data.message.like_deleted))
-            }        
-        } 
+        // ws.onmessage = (e)=>{
+        //     const data = JSON.parse(e.data)
+        //     if(data.type === "ping") return
+        //     if(data.type === "welcome") return
+        //     if(data.type === "confirm_subscription") return
+        //     if (data.message?.from_create_comment){
+        //         const payload = commentGetSetter({workOrderId: workOrderId, id: data.message.id})
+        //         // dispatch(getFetchAction(payload))
+        //     } else if (data.message?.from_delete_comment){
+        //         dispatch(deleteComment(data.message?.comment_deleted))
+        //     } else if (data.message?.from_create_like_for_comment){
+        //         const response = data.message.response
+        //         dispatch(addOrRemoveLikesFromComment(JSON.parse(response)))
+        //     } else if (data.message?.from_delete_like_from_comment){
+        //         dispatch(addOrRemoveLikesFromComment(data.message.like_deleted)) 
+        //     }else  if (data.message?.from_delete_reply ){
+        //         dispatch(removeReplyFromComment(data.message.reply_deleted))
+        //     }else if(data.message?.from_create_reply){
+        //         dispatch(addNewReply(JSON.parse(data.message.reply)))
+        //     }else if(data.message?.from_create_like_for_reply) {
+        //         const response = data.message.response
+        //         dispatch(addOrRemoveLikesFromReply(JSON.parse(response)))
+        //     } else if (data.message?.from_delete_like_from_reply){
+        //         dispatch(addOrRemoveLikesFromReply(data.message.like_deleted))
+        //     }        
+        // } 
 
     },[workOrderId])
 
