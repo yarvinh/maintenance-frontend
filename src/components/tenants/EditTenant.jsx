da995f421f447ebaff76192fb2f367295e951e35
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2024-10-31 22:27:53 -0400

    add message when edit tenent

--- src/components/tenants/EditTenant.jsx
+++ src/components/tenants/EditTenant.jsx
@@ -1,12 +1,14 @@
 import {useState} from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {useParams} from 'react-router-dom';
 import '../../styles/styles.css'
 import { patchFetchAction } from '../../actions/fetchActions';
 import { editTenantReceived } from '../../state/reducers/unitReducer';
+import ErrorsOrMsg from '../ErrosOrMsg';
 
 const EditTenant = ({tenant})=>{
     const {buildingId,unitId} = useParams()
+    const errorsOrMsg = useSelector(state => state.errorsOrMessages.errorsOrMessages)
     const dispatch = useDispatch()
     const [editTenant,setEditTenant] = useState({
         name: tenant.name,
@@ -21,7 +23,7 @@ const EditTenant = ({tenant})=>{
         })
     }
 
-    let handleOnSubmit = (e,type) =>{
+    let handleOnSubmit = (e) =>{
         e.preventDefault()
         const payload = {
             payload: {tenant: editTenant},
@@ -33,11 +35,12 @@ const EditTenant = ({tenant})=>{
 
     return (
         <>
-            <form onSubmit={(e)=>handleOnSubmit(e,"name")} className='accordion'>
+            <form onSubmit={handleOnSubmit}>
                 <input onChange={handleOnChange} placeholder={tenant.name} name="name" className="edit-input" type="text" value={editTenant.name}/>
                 <input onChange={handleOnChange} placeholder={tenant.phone} name="phone" className="edit-input" type="text" value={editTenant.phone}/>
                 <input onChange={handleOnChange}  placeholder={tenant.email} name="email" className="edit-input" type="text" value={editTenant.email}/>
                 <button type='submit' className="standar-button accordion">Save</button>
+                {errorsOrMsg.from.includes("edit_tenant") && <ErrorsOrMsg {...(errorsOrMsg.errors ? { errors: errorsOrMsg.errors } :{msg: errorsOrMsg.msg })} />}
             </form>  
         </>
     )
